1b41f9b70219169c9f86c10b93a36d0a86f522f1
Chef Expeditor <chef-ci@chef.io>
2020-04-24 00:59:43 +0000

    Bump version to 0.0.78 by Chef Expeditor
    Obvious fix; these changes are the result of automation not creative thinking.

--- lib/version.rb
+++ lib/version.rb
@@ -1,3 +1,3 @@
 class Version
-  VERSION = "0.0.77"
+  VERSION = "0.0.78"
 end
--- omnibus-harmony/config/projects/harmony.rb
+++ omnibus-harmony/config/projects/harmony.rb
@@ -6,7 +6,7 @@ license_file "LICENSE"
 
 install_dir "#{default_root}/#{name}"
 
-build_version "0.0.77"
+build_version "0.0.78"
 build_iteration 1
 
 # creates required build directories
